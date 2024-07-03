3a0778aa98cd0b0f8599080d7e75c88164396946
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-03 11:47:23 -0600

    Add footer to app and guest layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+  <div class="w-full px-4 py-6 mx-auto bg-gray-100 sm:px-6 lg:px-8">
+    <div class="text-sm leading-tight text-center text-gray-700">
+      © {{ date('Y') }} <a href="https://github.com/j2deme" target="_blank"
+        class="text-indigo-600 hover:text-indigo-800">J2deme</a>. Powered by <a href="https://laravel.com/"
+        class="text-indigo-600 hover:text-indigo-800" target="_blank">Laravel</a> v{{
+      Illuminate\Foundation\Application::VERSION }} & <a href="https://tallstack.dev/" target="_blank"
+        class="text-indigo-600 hover:text-indigo-800">TALL
+        Stack</a>.
+
+    </div>
+  </div>
+</footer>
