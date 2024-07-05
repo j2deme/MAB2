2fb200190545779a87171f45e70020b3b175f6bf
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-05 17:16:54 -0600

    Update Semestre form fields with livewire

--- resources/views/livewire/semestre/form.blade.php
+++ resources/views/livewire/semestre/form.blade.php
@@ -1,70 +1,74 @@
-<div class="space-y-6">
-    
-    <div>
-        <x-input-label for="clave" :value="__('Clave')"/>
-        <x-text-input wire:model="form.clave" id="clave" name="clave" type="text" class="mt-1 block w-full" autocomplete="clave" placeholder="Clave"/>
-        @error('form.clave')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+<div class="space-y-4">
+    <x-errors />
+    <div class="grid grid-cols-2 gap-6">
+        <div>
+            <x-input wire:model.defer="form.clave" id="clave" name="clave" class="" :label="__('Clave')"
+                placeholder="{{ date('Yn') }}" />
+        </div>
+        <div>
+            <x-input wire:model.defer="form.nombre" id="nombre" name="nombre" class="" :label="__('Nombre corto')"
+                placeholder="Ago - Dic {{ date('Y') }}" />
+        </div>
     </div>
     <div>
-        <x-input-label for="nombre" :value="__('Nombre')"/>
-        <x-text-input wire:model="form.nombre" id="nombre" name="nombre" type="text" class="mt-1 block w-full" autocomplete="nombre" placeholder="Nombre"/>
-        @error('form.nombre')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+        <x-input wire:model.defer="form.nombre_completo" id="nombre_completo" name="nombre_completo" class=""
+            :label="__('Nombre Completo')" placeholder="Agosto - Diciembre {{ date('Y') }}" />
     </div>
-    <div>
-        <x-input-label for="nombre_completo" :value="__('Nombre Completo')"/>
-        <x-text-input wire:model="form.nombre_completo" id="nombre_completo" name="nombre_completo" type="text" class="mt-1 block w-full" autocomplete="nombre_completo" placeholder="Nombre Completo"/>
-        @error('form.nombre_completo')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+    <div class="flex items-center gap-4">
+        <h2 class="text-lg font-semibold leading-tight text-gray-800">
+            {{ __('Fechas') }}
+        </h2>
     </div>
-    <div>
-        <x-input-label for="inicio_altas" :value="__('Inicio Altas')"/>
-        <x-text-input wire:model="form.inicio_altas" id="inicio_altas" name="inicio_altas" type="text" class="mt-1 block w-full" autocomplete="inicio_altas" placeholder="Inicio Altas"/>
-        @error('form.inicio_altas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+    <div class="grid grid-cols-2 gap-6">
+        <div>
+            <x-datetime-picker wire:model.defer="form.inicio_altas" id="inicio_altas" name="inicio_altas"
+                autocomplete="false" placeholder="Inicio altas" :label="__('Inicio Altas')" without-time
+                display-format="DD-MM-YYYY" />
+        </div>
+        <div>
+            <x-datetime-picker wire:model.defer="form.fin_altas" id="fin_altas" name="fin_altas" autocomplete="false"
+                placeholder="Fin altas" :label="__('Fin Altas')" without-time display-format="DD-MM-YYYY" />
+        </div>
     </div>
-    <div>
-        <x-input-label for="fin_altas" :value="__('Fin Altas')"/>
-        <x-text-input wire:model="form.fin_altas" id="fin_altas" name="fin_altas" type="text" class="mt-1 block w-full" autocomplete="fin_altas" placeholder="Fin Altas"/>
-        @error('form.fin_altas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+    <div class="grid grid-cols-2 gap-6">
+        <div>
+            <x-datetime-picker wire:model.defer="form.inicio_bajas" id="inicio_bajas" name="inicio_bajas"
+                autocomplete="false" placeholder="Inicio bajas" :label="__('Inicio Bajas')" without-time
+                display-format="DD-MM-YYYY" />
+        </div>
+        <div>
+            <x-datetime-picker wire:model.defer="form.fin_bajas" id="fin_bajas" name="fin_bajas" autocomplete="false"
+                placeholder="Fin bajas" :label="__('Fin Bajas')" without-time display-format="DD-MM-YYYY" />
+        </div>
     </div>
-    <div>
-        <x-input-label for="inicio_bajas" :value="__('Inicio Bajas')"/>
-        <x-text-input wire:model="form.inicio_bajas" id="inicio_bajas" name="inicio_bajas" type="text" class="mt-1 block w-full" autocomplete="inicio_bajas" placeholder="Inicio Bajas"/>
-        @error('form.inicio_bajas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+    <div class="flex items-center gap-4">
+        <h2 class="text-lg font-semibold leading-tight text-gray-800">
+            {{ __('Otros') }}
+        </h2>
     </div>
-    <div>
-        <x-input-label for="fin_bajas" :value="__('Fin Bajas')"/>
-        <x-text-input wire:model="form.fin_bajas" id="fin_bajas" name="fin_bajas" type="text" class="mt-1 block w-full" autocomplete="fin_bajas" placeholder="Fin Bajas"/>
-        @error('form.fin_bajas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
-    </div>
-    <div>
-        <x-input-label for="max_altas" :value="__('Max Altas')"/>
-        <x-text-input wire:model="form.max_altas" id="max_altas" name="max_altas" type="text" class="mt-1 block w-full" autocomplete="max_altas" placeholder="Max Altas"/>
-        @error('form.max_altas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
-    </div>
-    <div>
-        <x-input-label for="activo" :value="__('Activo')"/>
-        <x-text-input wire:model="form.activo" id="activo" name="activo" type="text" class="mt-1 block w-full" autocomplete="activo" placeholder="Activo"/>
-        @error('form.activo')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+    <div class="grid grid-cols-2 gap-6">
+        <div>
+            <x-input wire:model.defer="form.max_altas" id="max_altas" name="max_altas" :label="__('Max Altas')"
+                type="number" min="0" max="10" placeholder="5" />
+        </div>
+        <div>
+            <x-toggle wire.model.defer="form.activo" id="activo" name="activo" :label="__('Activo')" lg />
+        </div>
     </div>
 
     <div class="flex items-center gap-4">
-        <x-primary-button>Submit</x-primary-button>
+        <x-primary-button>
+            <x-phosphor.icons::regular.floppy-disk class="w-4 h-4 mr-2" />
+            {{ __('Guardar') }}
+        </x-primary-button>
+
+        <x-link label="Cancelar" :href="route('semestres.index')" />
+
+        @if ($errors->any())
+        <div class="flex text-sm text-red-800 flex-items">
+            <x-phosphor.icons::regular.warning class="w-4 h-4 mr-2" />
+            {{ __('Corrija los errores antes de continuar') }}
+        </div>
+        @endif
     </div>
 </div>
