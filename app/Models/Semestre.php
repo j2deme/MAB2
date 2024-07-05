a006e90854f539442cc0c8edd5e9b87323054b51
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-05 17:11:56 -0600

    Update Semestre model with periodo_altas and periodo_bajas attributes

--- app/Models/Semestre.php
+++ app/Models/Semestre.php
@@ -19,9 +19,26 @@ use Illuminate\Database\Eloquent\Model;
  * @property $activo
  * @property $created_at
  * @property $updated_at
- *
  * @package App
  * @mixin \Illuminate\Database\Eloquent\Builder
+ * @property-read string $periodo_altas
+ * @property-read string $periodo_bajas
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereActivo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereClave($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereFinAltas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereFinBajas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereInicioAltas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereInicioBajas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereMaxAltas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereNombre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereNombreCompleto($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Semestre whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Semestre extends Model
 {
@@ -42,5 +59,14 @@ class Semestre extends Model
      */
     protected $casts = ['inicio_altas' => 'datetime', 'fin_altas' => 'datetime', 'inicio_bajas' => 'datetime', 'fin_bajas' => 'datetime', 'max_altas' => 'integer', 'activo' => 'boolean'];
 
+    public function getPeriodoAltasAttribute(): string
+    {
+        return $this->inicio_altas->format('d/m/Y') . ' - ' . $this->fin_altas->format('d/m/Y');
+    }
+
+    public function getPeriodoBajasAttribute(): string
+    {
+        return $this->inicio_bajas->format('d/m/Y') . ' - ' . $this->fin_bajas->format('d/m/Y');
+    }
 
 }
