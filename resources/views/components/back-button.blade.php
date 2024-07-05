5417f95c8d2d3c0860905bb98dedb0d670ec1bc7
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-05 15:32:37 -0600

    Update Spanish translation and back button style

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,7 +1,7 @@
 @props(['url' => null])
 
 <a type="button" href="{{ ($url) ? $url : route('dashboard') }}"
-  class="block px-3 py-2 text-sm font-semibold text-center text-white rounded-md shadow-sm bg-primary-600 hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">
+  class="block px-3 py-2 text-sm font-semibold text-center text-white rounded-md shadow-sm bg-secondary-600 hover:bg-secondary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-600">
   <x-phosphor.icons::regular.caret-left class="inline-block w-5 h-5 mr-2 -mt-1" />
   {{ __('Back') }}
 </a>
