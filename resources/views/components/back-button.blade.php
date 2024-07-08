03b9762eb89187290a7dbedc3c5a8c880a2009ba
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-08 16:46:58 -0600

    Update back-button component to use wire:navigate
    The back-button component has been updated to use the `wire:navigate` directive instead of the `href` attribute. This change improves the navigation behavior and ensures compatibility with Livewire.

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,6 +1,6 @@
 @props(['url' => null])
 
-<a type="button" href="{{ ($url) ? $url : route('dashboard') }}"
+<a type="button" wire:navigate href="{{ ($url) ? $url : route('dashboard') }}"
   class="block px-3 py-2 text-sm font-semibold text-center text-white rounded-md shadow-sm bg-secondary-600 hover:bg-secondary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-600">
   <x-icon name="caret-left" class="inline-block w-5 h-5 mr-2 -mt-1" />
   {{ __('Back') }}
