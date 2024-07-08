c411a042ae5a242e808425a0889ed01c9177de1c
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-07 21:19:03 -0600

    Remove delete route for semestres in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('/semestres/create', 'semestres.create')->name('semestres.create');
     Volt::route('/semestres/show/{semestre}', 'semestres.show')->name('semestres.show');
     Volt::route('/semestres/update/{semestre}', 'semestres.edit')->name('semestres.edit');
-    Volt::route('/semestres/delete/{semestre}', 'semestres.delete')->name('semestres.delete');
 });
 
 
