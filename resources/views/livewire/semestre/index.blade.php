f74860e6461a929d79152a44843c450d49922ca1
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-08 17:27:21 -0600

    Update Semestre index page with new "Agregar semestre" button

--- resources/views/livewire/semestre/index.blade.php
+++ resources/views/livewire/semestre/index.blade.php
@@ -24,9 +24,10 @@ state([
             <p class="mt-2 text-sm text-gray-700">A list of all the {{ __('Semestres') }}.</p>
           </div>
           <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <a type="button" wire:navigate href="{{ route('semestres.create') }}"
-              class="block px-3 py-2 text-sm font-semibold text-center text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
-              new</a>
+            <x-primary-button wire:navigate href="{{ route('semestres.create') }}" class="">
+              <x-icon name="plus" class="w-4 h-4 mr-2" />
+              {{ __('Agregar semestre') }}
+            </x-primary-button>
           </div>
         </div>
 
