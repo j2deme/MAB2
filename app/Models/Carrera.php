98b5ae8a0efbbb3a8dd9a7e56bd26396ec64e1ca
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-09 13:26:49 -0600

    Add Materias relationship to Carrera

--- app/Models/Carrera.php
+++ app/Models/Carrera.php
@@ -13,13 +13,24 @@ use Illuminate\Database\Eloquent\Model;
  * @property $nombre
  * @property $created_at
  * @property $updated_at
- *
  * @package App
  * @mixin \Illuminate\Database\Eloquent\Builder
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereClaveInterna($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereNombre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereSiglas($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Carrera whereUpdatedAt($value)
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Materia> $materias
+ * @property-read int|null $materias_count
+ * @mixin \Eloquent
  */
 class Carrera extends Model
 {
-    
+
     protected $perPage = 20;
 
     /**
@@ -29,5 +40,8 @@ class Carrera extends Model
      */
     protected $fillable = ['siglas', 'clave_interna', 'nombre'];
 
-
+    public function materias()
+    {
+        return $this->hasMany(Materia::class);
+    }
 }
