f177126077bcc98a1054d4989d6fd1ca854844dc
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-09 16:21:51 -0600

    Add API route to get the 'id' and 'nombre' from carreras

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Carrera;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::name('api.')->group(function () {
+    Route::get('/carreras', function (Request $request) {
+        return Carrera::all('id', 'nombre')->toJson();
+    })->name('carreras.index');
+});
 
