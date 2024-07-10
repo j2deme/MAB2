fb522024a620a55c223c0769160b1f5b785b343c
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-07-09 22:15:04 -0600

    Remove laravel/folio dependency and unused provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\FolioServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
     Barryvdh\Debugbar\ServiceProvider::class,
     Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
