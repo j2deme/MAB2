8dfae06abb7f98d6f7b6bbaca227d0f6031bf2ff
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-17 14:16:13 -0600

    Set default values for semestre and grupo properties in Create component
    The code changes in the `Create` component set default values for the `semestre` and `grupo` properties. The `semestre` property is set to the active semester retrieved from the `Semestre` model, and the `grupo` properties `is_disponible` and `is_paralelizable` are set to `true` and `false` respectively. This ensures that the form is pre-populated with the correct values when creating a new grupo.

--- app/Livewire/Grupos/Create.php
+++ app/Livewire/Grupos/Create.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Grupos;
 
 use App\Livewire\Forms\GrupoForm;
 use App\Models\Grupo;
+use App\Models\Semestre;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 use WireUi\Traits\WireUiActions;
@@ -12,9 +13,14 @@ class Create extends Component
 {
     use WireUiActions;
     public GrupoForm $form;
+    public Semestre $semestre;
 
     public function mount(Grupo $grupo)
     {
+        $semestre                = Semestre::where('activo', true)->first();
+        $grupo->semestre_id      = $semestre->id;
+        $grupo->is_disponible    = true;
+        $grupo->is_paralelizable = false;
         $this->form->setGrupoModel($grupo);
     }
 
