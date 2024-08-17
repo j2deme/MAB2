57e33aa2bfc78fa260c80b916296e68a792bc2aa
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-17 14:10:20 -0600

    Add validation rules and error messages to GrupoForm
    The commit adds validation rules and error messages to the `GrupoForm` class in order to enforce data integrity and provide meaningful feedback to users when submitting the form.

--- app/Livewire/Forms/GrupoForm.php
+++ app/Livewire/Forms/GrupoForm.php
@@ -4,11 +4,12 @@ namespace App\Livewire\Forms;
 
 use App\Models\Grupo;
 use Livewire\Form;
+use Illuminate\Validation\Rule;
 
 class GrupoForm extends Form
 {
     public ?Grupo $grupoModel;
-    
+
     public $siglas = '';
     public $semestre_id = '';
     public $materia_id = '';
@@ -18,22 +19,38 @@ class GrupoForm extends Form
     public function rules(): array
     {
         return [
-			'siglas' => 'required|string',
-			'semestre_id' => 'required',
-			'materia_id' => 'required',
-			'is_disponible' => 'required',
-			'is_paralelizable' => 'required',
+            'siglas' => [
+                'required',
+                'string',
+                'max:5',
+                // Rule::unique('grupos', 'siglas')
+                //     ->ignore($this->grupoModel->id)
+                //     ->where('materia_id', $this->grupoModel->materia_id)
+            ],
+            'semestre_id' => 'required',
+            'materia_id' => 'required',
+            'is_disponible' => 'required|boolean',
+            'is_paralelizable' => 'required|boolean',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'siglas.unique' => 'Ya existe un grupo con estas siglas para la materia seleccionada.',
+            'semestre_id.required' => 'El campo semestre es obligatorio.',
+            'materia_id.required' => 'El campo materia es obligatorio.'
         ];
     }
 
     public function setGrupoModel(Grupo $grupoModel): void
     {
         $this->grupoModel = $grupoModel;
-        
-        $this->siglas = $this->grupoModel->siglas;
-        $this->semestre_id = $this->grupoModel->semestre_id;
-        $this->materia_id = $this->grupoModel->materia_id;
-        $this->is_disponible = $this->grupoModel->is_disponible;
+
+        $this->siglas           = $this->grupoModel->siglas;
+        $this->semestre_id      = $this->grupoModel->semestre_id;
+        $this->materia_id       = $this->grupoModel->materia_id;
+        $this->is_disponible    = $this->grupoModel->is_disponible;
         $this->is_paralelizable = $this->grupoModel->is_paralelizable;
     }
 
