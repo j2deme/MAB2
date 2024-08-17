d3da6b879d3fce0c9204e7e4db888ba4483a740d
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-17 12:39:07 -0600

    Add endpoint to retrieve list of materias with additional information
    This commit adds a new endpoint to retrieve a list of materias with additional information. The endpoint returns the id, nombre_completo, clave, and nombre_visual of each materia.
    
    The nombre_visual is generated by concatenating the clave and nombre_completo.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Carrera;
+use App\Models\Materia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Carrera;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -12,5 +13,13 @@ Route::name('api.')->group(function () {
     Route::get('/carreras', function (Request $request) {
         return Carrera::all('id', 'nombre')->toJson();
     })->name('carreras.index');
+
+    Route::get('/materias', function (Request $request) {
+        $materias = Materia::all('id', 'nombre_completo', 'clave');
+        $materias->each(function ($materia) {
+            $materia->nombre_visual = "{$materia->clave} - {$materia->nombre_completo}";
+        });
+        return $materias->toJson();
+    })->name('materias.index');
 });
 
