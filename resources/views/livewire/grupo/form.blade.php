10680fc6c181b156986323cbf598ef20755ac620
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-17 14:16:32 -0600

    Update grupo form layout and error handling
    The code changes in the `grupo/form.blade.php` file update the layout of the form and add error handling for the `semestre_id` and `materia_id` fields. This improves the user experience by providing a more intuitive form layout and displaying error messages when there are validation issues.

--- resources/views/livewire/grupo/form.blade.php
+++ resources/views/livewire/grupo/form.blade.php
@@ -1,39 +1,47 @@
 <div class="space-y-6">
-    
-    <div>
-        <x-input wire:model.defer='form.siglas' id='siglas' name='siglas' class='' :label="__('Siglas')" placeholder='Siglas' />
+    <x-errors />
+    <div class="hidden">
+        <x-input wire:model.defer='form.semestre_id' id='semestre_id' name='semestre_id' class=''
+            :label="__('Semestre Id')" placeholder='Semestre Id' />
 
-        @error('form.siglas')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+        {{-- @error('form.semestre_id')
+        <x-input-error class="mt-2" :messages="$message" />
+        @enderror --}}
     </div>
     <div>
-        <x-input wire:model.defer='form.semestre_id' id='semestre_id' name='semestre_id' class='' :label="__('Semestre Id')" placeholder='Semestre Id' />
+        <x-select wire:model.defer="form.materia_id" id="materia_id" name="materia_id" label="Materia"
+            placeholder="Selecciona una materia" :async-data="route('api.materias.index')" option-label="nombre_visual"
+            option-value="id" />
 
-        @error('form.semestre_id')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+        {{-- @error('form.materia_id')
+        <x-input-error class="mt-2" :messages="$message" />
+        @enderror --}}
     </div>
-    <div>
-        <x-input wire:model.defer='form.materia_id' id='materia_id' name='materia_id' class='' :label="__('Materia Id')" placeholder='Materia Id' />
+    <div class="grid grid-cols-3 gap-6">
+        <div>
+            <x-input wire:model.defer='form.siglas' id='siglas' name='siglas' class='' :label="__('Siglas')"
+                placeholder='Siglas' />
 
-        @error('form.materia_id')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
-    </div>
-    <div>
-        <x-input wire:model.defer='form.is_disponible' id='is_disponible' name='is_disponible' class='' :label="__('Is Disponible')" placeholder='Is Disponible' />
+            {{-- @error('form.siglas')
+            <x-input-error class="mt-2" :messages="$message" />
+            @enderror --}}
+        </div>
+        <div>
+            <x-toggle wire:model.defer="form.is_disponible" id="is_disponible" name="is_disponible"
+                :label="__('¿Disponible para selección?')" lg checked />
 
-        @error('form.is_disponible')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
-    </div>
-    <div>
-        <x-input wire:model.defer='form.is_paralelizable' id='is_paralelizable' name='is_paralelizable' class='' :label="__('Is Paralelizable')" placeholder='Is Paralelizable' />
+            {{-- @error('form.is_disponible')
+            <x-input-error class="mt-2" :messages="$message" />
+            @enderror --}}
+        </div>
+        <div>
+            <x-toggle wire:model.defer="form.is_paralelizable" id="is_paralelizable" name="is_paralelizable"
+                :label="__('¿Paralelizable?')" lg checked />
 
-        @error('form.is_paralelizable')
-            <x-input-error class="mt-2" :messages="$message"/>
-        @enderror
+            {{-- @error('form.is_paralelizable')
+            <x-input-error class="mt-2" :messages="$message" />
+            @enderror --}}
+        </div>
     </div>
 
     <div class="flex items-center gap-4">
