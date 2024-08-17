b78356a984e1e4ce9073a03d87f6ded47869004e
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-17 14:07:44 -0600

    Update grupo index page to use grupos-table livewire component

---
+++ app/Livewire/GruposTable.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Grupo;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Button;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Exportable;
+use PowerComponents\LivewirePowerGrid\Facades\Filter;
+use PowerComponents\LivewirePowerGrid\Footer;
+use PowerComponents\LivewirePowerGrid\Header;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridFields;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\WithExport;
+use Illuminate\Support\Facades\Blade;
+
+final class GruposTable extends PowerGridComponent
+{
+    use WithExport;
+
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('grupos')
+                ->striped()
+                ->type(Exportable::TYPE_XLS),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        return Grupo::query()->with('materia');
+    }
+
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id')
+            ->add('siglas')
+            ->add('semestre_id')
+            ->add('materia_id')
+            ->add('materia_nombre', fn(Grupo $model) => e($model->materia->nombre_completo . ' (' . $model->materia->clave . ')'))
+            ->add('is_disponible')
+            ->add('disponible_icon', fn(Grupo $model) => $model->is_disponible ? Blade::render('<x-icon bold name="check" class="w-5 h-5 text-green-500" />') : Blade::render('<x-icon bold name="x" class="w-5 h-5 text-red-500" />'))
+            ->add('is_paralelizable')
+            ->add('paralelizable_icon', fn(Grupo $model) => $model->is_paralelizable ? Blade::render('<x-icon bold name="letter-circle-p" class="w-5 h-5 text-blue-500" />') : Blade::render('<x-icon bold name="prohibit" class="w-5 h-5 text-gray-500" />'))
+            ->add('created_at');
+    }
+
+    public function columns(): array
+    {
+        return [
+            // Column::make('Materia id', 'materia_id'),
+            Column::make('Materia', 'materia_nombre', 'materia_id')
+                ->searchable(),
+
+            Column::make('Grupo', 'siglas')
+                ->contentClasses('flex items-center justify-center')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Disponible', 'is_disponible')
+                ->hidden(),
+
+            Column::make('Disponible', 'disponible_icon', 'is_disponible')
+                ->contentClasses('flex items-center justify-center')
+                ->visibleInExport(false)
+                ->sortable(),
+
+            Column::make('Paralelizable', 'paralelizable_icon', 'is_paralelizable')
+                ->contentClasses('flex items-center justify-center')
+                ->visibleInExport(false)
+                ->sortable(),
+
+            Column::action('')
+                ->title(Blade::render('<x-icon name="gear" class="w-5 h-5 text-gray-600" />')),
+        ];
+    }
+
+    public function filters(): array
+    {
+        $materias = Grupo::query()->with('materia')->groupBy('materia_id')->select('materia_id')->get()->map(function ($materia) {
+            return [
+                'label' => $materia->materia->nombre_completo . ' (' . $materia->materia->clave . ')',
+                'value' => $materia->materia_id,
+            ];
+        });
+
+        $siglas = Grupo::query()->groupBy('siglas')->select('siglas')->get()->map(function ($sigla) {
+            return [
+                'label' => $sigla->siglas,
+                'value' => $sigla->siglas,
+            ];
+        });
+
+        return [
+            Filter::select('materia_id')
+                ->dataSource($materias)
+                ->optionLabel('label')
+                ->optionValue('value'),
+            Filter::select('siglas')
+                ->dataSource($siglas)
+                ->optionLabel('label')
+                ->optionValue('value'),
+            Filter::select('is_disponible')
+                ->dataSource([
+                    ['label' => 'Sí', 'value' => '1'],
+                    ['label' => 'No', 'value' => '0'],
+                ])
+                ->optionLabel('label')
+                ->optionValue('value'),
+            Filter::select('is_paralelizable')
+                ->dataSource([
+                    ['label' => 'Sí', 'value' => '1'],
+                    ['label' => 'No', 'value' => '0'],
+                ])
+                ->optionLabel('label')
+                ->optionValue('value'),
+        ];
+    }
+
+    #[\Livewire\Attributes\On('delete')]
+    public function delete($rowId): void
+    {
+        Grupo::query()->find($rowId)->delete();
+
+        $this->notification()->error('Registro eliminado', 'Grupo eliminado correctamente.');
+
+        $this->refresh();
+    }
+
+    public function actions(Grupo $row): array
+    {
+        return [
+            Button::add('actions')
+                ->bladeComponent('row-actions', [
+                    'model' => 'grupos',
+                    'id' => $row->id
+                ]),
+        ];
+    }
+
+    /*
+    public function actionRules($row): array
+    {
+       return [
+            // Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($row) => $row->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
--- resources/views/livewire/grupo/index.blade.php
+++ resources/views/livewire/grupo/index.blade.php
@@ -11,7 +11,6 @@
                 <div class="sm:flex sm:items-center">
                     <div class="sm:flex-auto">
                         <h1 class="text-base font-semibold leading-6 text-gray-900">{{ __('Grupos') }}</h1>
-                        <p class="mt-2 text-sm text-gray-700">A list of all the {{ __('Grupos') }}.</p>
                     </div>
                     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                         <x-primary-button wire:navigate href="{{ route('grupos.create') }}" class="">
@@ -21,56 +20,8 @@
                     </div>
                 </div>
 
-                <div class="flow-root">
-                    <div class="mt-8 overflow-x-auto">
-                        <div class="inline-block min-w-full py-2 align-middle">
-                            <table class="w-full divide-y divide-gray-300">
-                                <thead>
-                                <tr>
-                                    <th scope="col" class="py-3 pl-4 pr-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase">No</th>
-                                    
-									<th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Siglas</th>
-									<th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Semestre Id</th>
-									<th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Materia Id</th>
-									<th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Is Disponible</th>
-									<th scope="col" class="py-3 pl-4 pr-3 text-left text-xs font-semibold uppercase tracking-wide text-gray-500">Is Paralelizable</th>
-
-                                    <th scope="col" class="px-3 py-3 text-xs font-semibold tracking-wide text-left text-gray-500 uppercase"></th>
-                                </tr>
-                                </thead>
-                                <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($grupos as $grupo)
-                                    <tr class="even:bg-gray-50" wire:key="{{ $grupo->id }}">
-                                        <td class="py-4 pl-4 pr-3 text-sm font-semibold text-gray-900 whitespace-nowrap">{{ ++$i }}</td>
-                                        
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $grupo->siglas }}</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $grupo->semestre_id }}</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $grupo->materia_id }}</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $grupo->is_disponible }}</td>
-										<td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $grupo->is_paralelizable }}</td>
-
-                                        <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                            <a wire:navigate href="{{ route('grupos.show', $grupo->id) }}" class="mr-2 font-bold text-gray-600 hover:text-gray-900">{{ __('Show') }}</a>
-                                            <a wire:navigate href="{{ route('grupos.edit', $grupo->id) }}" class="mr-2 font-bold text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
-                                            <button
-                                                class="font-bold text-red-600 hover:text-red-900"
-                                                type="button"
-                                                wire:click="delete({{ $grupo->id }})"
-                                                wire:confirm="Are you sure you want to delete?"
-                                            >
-                                                {{ __('Delete') }}
-                                            </button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-
-                            <div class="px-4 mt-4">
-                                {!! $grupos->withQueryString()->links() !!}
-                            </div>
-                        </div>
-                    </div>
+                <div class="flow-root mt-4">
+                    <livewire:grupos-table />
                 </div>
             </div>
         </div>
