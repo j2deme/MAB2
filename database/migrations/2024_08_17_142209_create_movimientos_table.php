6dfeb2ca213e15d5398dfcfe9d3288cb3522a2cc
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-20 17:56:45 -0600

    Update 'movimientos' table migration to make 'carrera_id' column nullable
    The code changes in the '2024_08_17_142209_create_movimientos_table.php' migration modify the 'carrera_id' column in the 'movimientos' table. The column is updated to be nullable, allowing for cases where a movement does not have a corresponding career. This change provides flexibility in the data model and aligns with the relationship between movements and careers.

--- database/migrations/2024_08_17_142209_create_movimientos_table.php
+++ database/migrations/2024_08_17_142209_create_movimientos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('semestre_id')->constrained();
-            $table->foreignId('carrera_id')->constrained();
+            $table->foreignId('carrera_id')->nullable()->constrained();
             $table->foreignId('grupo_id')->constrained();
             $table->enum('tipo', array_column(MovesType::cases(), 'value'));
             $table->enum('estatus', array_column(MovesStatus::cases(), 'value'))->default(MovesStatus::REGISTRADO->value);
