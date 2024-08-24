224649c2ec56d2ca94a1d976f74bf9859504c8df
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-23 21:56:10 -0600

    Refactor ParaleloIcon component to use minus icon when not active

--- resources/views/components/paralelo-icon.blade.php
+++ resources/views/components/paralelo-icon.blade.php
@@ -3,5 +3,5 @@
 @if ($paralelo)
 <x-icon bold name="letter-circle-p" class="w-5 h-5 text-blue-500" />
 @else
-<x-icon bold name="prohibit" class="w-5 h-5 text-gray-500" />
+<x-icon bold name="minus" class="w-5 h-5 text-gray-500" />
 @endif
