6c2b837403726e5b07399369f0efd0eec24da712
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-08-24 16:23:37 -0600

    Refactor  carrera-badge component to remove color shades on badges

--- resources/views/components/carrera-badge.blade.php
+++ resources/views/components/carrera-badge.blade.php
@@ -2,10 +2,11 @@
 
 @if (isset($carreras) and $carreras->count() > 0)
 @foreach ($carreras as $carrera)
-<x-badge label="{{ $carrera->siglas }}" class="bg-{{ $carrera->color }}" />
+<x-badge label="{{ preg_replace('/[^a-zA-Z]/', '', $carrera->siglas) }}"
+  color="{{ preg_replace('/[^a-zA-Z]/', '', $carrera->color) }}" />
 @endforeach
 @elseif (isset($carrera))
-<x-badge label="{{ $carrera->siglas }}" class="bg-{{ $carrera->color }}" />
+<x-badge label="{{ $carrera->siglas }}" color="{{ preg_replace('/[^a-zA-Z]/', '', $carrera->color) }}" />
 @else
 N/A
 @endif
