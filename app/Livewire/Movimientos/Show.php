e92d984aa0ff4cca080e677aecfdbbd8019a597d
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-09-01 21:59:46 -0600

    Update Show component to display in a more intuitive way the information from the movimiento

--- app/Livewire/Movimientos/Show.php
+++ app/Livewire/Movimientos/Show.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Movimientos;
 
 use App\Livewire\Forms\MovimientoForm;
 use App\Models\Movimiento;
+use App\Models\Semestre;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -11,9 +12,12 @@ class Show extends Component
 {
     public MovimientoForm $form;
 
+    public Semestre $semestre;
+
     public function mount(Movimiento $movimiento)
     {
         $this->form->setMovimientoModel($movimiento);
+        $this->semestre = $this->form->movimientoModel->semestre;
     }
 
     #[Layout('layouts.app')]
--- resources/views/livewire/movimiento/show.blade.php
+++ resources/views/livewire/movimiento/show.blade.php
@@ -1,95 +1,92 @@
 <x-slot name="header">
-    <h2 class="text-xl font-semibold leading-tight text-gray-800">
-        {{ $movimiento->name ?? __('Show') . " " . __('Movimiento') }}
-    </h2>
+  <h2 class="text-xl font-semibold leading-tight text-gray-800">
+    {{ Str::title($movimiento->nombre) ?? __('Show') . " " . __('Solicitud') }}
+  </h2>
 </x-slot>
 
 <div class="py-6">
-    <div class="max-w-full mx-auto space-y-6 sm:px-6 lg:px-8">
-        <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
-            <div class="w-full">
-                <div class="sm:flex sm:items-center">
-                    <div class="sm:flex-auto">
-                        <h1 class="text-base font-semibold leading-6 text-gray-900">{{ __('Show') }} Movimiento</h1>
-                    </div>
-                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        @include('components.back-button', ['url' => route('movimientos.index')])
-                    </div>
-                </div>
-
-                <div class="flow-root">
-                    <div class="mt-8 overflow-x-auto">
-                        <div class="inline-block min-w-full py-2 align-middle">
-                            <div class="mt-6 border-t border-gray-100">
-                                <dl class="divide-y divide-gray-100">
-
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">User Id</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->user_id }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Semestre Id</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->semestre_id }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Carrera Id</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->carrera_id }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Grupo Id</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->grupo_id }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Tipo</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->tipo }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Estatus</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->estatus }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Motivo</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->motivo }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Motivo Adicional</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->motivo_adicional }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Respuesta</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->respuesta }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Respuesta Adicional</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->respuesta_adicional }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Asociado Id</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->asociado_id }}</dd>
-                                    </div>
-                                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <dt class="text-sm font-medium leading-6 text-gray-900">Is Paralelo</dt>
-                                        <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{
-                                            $movimiento->is_paralelo }}</dd>
-                                    </div>
-
-                                </dl>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="max-w-full mx-auto space-y-6 sm:px-6 lg:px-8">
+    <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
+      <div class="w-full">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-base font-semibold leading-6 text-gray-900">Solicitud</h1>
+            <p class="mt-2 text-sm text-gray-700">{{ $semestre->nombre_completo }}</p>
+          </div>
+          <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            @include('components.back-button')
+            {{-- , ['url' => route('movimientos.index')] --}}
+          </div>
         </div>
+
+        <div class="flow-root">
+          <div class="mt-8 overflow-x-auto">
+            <div class="inline-block w-1/2 py-2 align-middle">
+              <x-card class="border-2 border-{{ $movimiento->grupo->carrera->color }} text-sm" shadow="md">
+                <x-slot name="title" class="w-full">
+                  <div class="grid grid-cols-2">
+                    <div class="place-self-start">
+                      {{ $movimiento->grupo->materia->clave }}
+                    </div>
+                    <div class="place-self-end">
+                      {{ $movimiento->user->username }}
+                    </div>
+                  </div>
+                </x-slot>
+                {{ $movimiento->grupo->materia->nombre_completo }} ({{ $movimiento->grupo->siglas }})
+
+                <x-slot name="footer" class="w-full">
+                  <div class="grid grid-cols-3">
+                    <div class="place-self-start">
+                      @if ($movimiento->is_paralelo)
+                      @include('components.carrera-badge', ['carrera' => $movimiento->user->carreras->first(),
+                      'paralelo' =>
+                      $movimiento->carrera])
+                      @else
+                      @include('components.carrera-badge', ['carrera' => $movimiento->user->carreras->first()])
+                      @endif
+                    </div>
+                    <div class="place-self-center">
+                      @include('components.movimiento-tipo-icon', ['tipo' =>
+                      $movimiento->tipo->value])
+                    </div>
+                    <div class="place-self-end">
+                      @includeWhen($movimiento->is_paralelo,'components.paralelo-icon',
+                      ['paralelo' =>
+                      $movimiento->is_paralelo])
+                    </div>
+                  </div>
+                </x-slot>
+              </x-card>
+
+              <h2 class="mt-8 text-base font-semibold leading-6 text-gray-900">Motivo</h2>
+              <x-card title="{{ $movimiento->motivo }}" shadow="md" class="mt-4">
+                @if (!Str($movimiento->motivo_adicional)->isEmpty())
+                <p class="text-sm">{{ $movimiento->motivo_adicional }}</p>
+                @endif
+              </x-card>
+
+              <h2 class="mt-8 text-base font-semibold leading-6 text-gray-900">Respuesta</h2>
+              <x-card shadow="md" class="mt-4 border-2 border-{{ $movimiento->estatus->color() }}-500">
+                <x-slot name="title" class="w-full">
+                  <div class="grid grid-cols-2">
+                    <div class="place-self-start">
+                      {{ $movimiento->respuesta }}
+                    </div>
+                    <div class="place-self-end">
+                      @include('components.movimiento-estatus-badge', ['estatus' => $movimiento->estatus])
+                    </div>
+                  </div>
+                </x-slot>
+
+                @if (!Str($movimiento->respuesta_adicional)->isEmpty())
+                <p class="text-sm">{{ $movimiento->respuesta_adicional }}</p>
+                @endif
+              </x-card>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
