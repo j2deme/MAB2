841856b855816ea3a4505fa4f28968780857a0a8
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-09-01 21:22:17 -0600

    Update MovimientoRowActions component to conditionally display edit button based on user role and model's is_paralelo property

--- resources/views/components/movimiento-row-actions.blade.php
+++ resources/views/components/movimiento-row-actions.blade.php
@@ -4,7 +4,8 @@
   <x-button wire:navigate flat secondary interaction:solid href="{{ route('movimientos.show', $model->id) }}">
     <x-icon name="eye" class="w-5 h-5 -mx-2" />
   </x-button>
-  @if (!auth()->user()->es('Estudiante'))
+
+  @if (auth()->user()->es(['Administrador','Jefe']) or (auth()->user()->es('Coordinador') and !$model->is_paralelo))
   <x-button wire:navigate flat blue interaction:solid href="{{ route('movimientos.edit', $model->id) }}">
     <x-icon name="pencil-simple" class="w-5 h-5 -mx-2" />
   </x-button>
