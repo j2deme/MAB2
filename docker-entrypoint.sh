34decda070b7f4adf18c419738bd8368d8a98a3a
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2026-08-25 00:48:48 -0600

    Refactor docker-entrypoint.sh para optimizar la limpieza de cachés y ajustar permisos de archivos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Asegurar permisos correctos
+find storage/framework/views -maxdepth 1 -type f -name '*.php' -delete
 chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
+find storage bootstrap/cache -type d -exec chmod 2775 {} +
+find storage bootstrap/cache -type f -exec chmod 664 {} +
 
 # Limpiar cachés previas para evitar desincronización
 su -s /bin/bash www-data -c 'php artisan config:clear'
 su -s /bin/bash www-data -c 'php artisan route:clear'
 su -s /bin/bash www-data -c 'php artisan view:clear'
-find storage/framework/views -maxdepth 1 -type f -name '*.php' -delete
-
-# Optimizar solo lo necesario
-su -s /bin/bash www-data -c 'php artisan view:cache'
 
 # Iniciar Apache
 apache2-foreground
