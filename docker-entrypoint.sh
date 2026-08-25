ae2a7553943e89d3b0f205ea9b2d5826a3baaf57
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2026-08-24 22:27:28 -0600

    Refactor docker-entrypoint.sh para ejecutar comandos de Artisan como el usuario www-data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,13 @@ chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
 # Limpiar cachés previas para evitar desincronización
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
+su -s /bin/bash www-data -c 'php artisan config:clear'
+su -s /bin/bash www-data -c 'php artisan route:clear'
+su -s /bin/bash www-data -c 'php artisan view:clear'
+find storage/framework/views -maxdepth 1 -type f -name '*.php' -delete
 
 # Optimizar solo lo necesario
-php artisan view:cache
+su -s /bin/bash www-data -c 'php artisan view:cache'
 
 # Iniciar Apache
 apache2-foreground
